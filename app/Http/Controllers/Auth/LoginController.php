36653669c433fcef631c5c9061d38ecd5bf77b42
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2019-05-24 23:30:55 +0900

    ログインまわりのController差し戻し

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,16 +35,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    // ★ メソッド追加
-    protected function authenticated(Request $request, $user)
-    {
-        return $user;
-    }
-
-    protected function loggedOut(Request $request)
-    {
-        return response()->json();
     }
 }
