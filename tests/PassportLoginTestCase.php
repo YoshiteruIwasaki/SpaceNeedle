603c4f308955e9396649b764c1705836129ccc42
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2019-05-24 23:51:33 +0900

    unittestでsqliteを使うように変更

--- tests/PassportLoginTestCase.php
+++ tests/PassportLoginTestCase.php
@@ -6,11 +6,13 @@ use App\User;
 use Laravel\Passport\ClientRepository;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
 
 class PassportLoginTestCase extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
+    //use DatabaseTransactions;
 
     protected $headersWithToken = [];
     protected $headersWithoutToken = [];
