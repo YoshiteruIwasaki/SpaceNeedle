1f590aeec00b913f79cfa5de3ef57b4b82bd09ff
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2019-06-09 11:26:32 +0900

    apache conf

--- config/logging.php
+++ config/logging.php
@@ -41,8 +41,7 @@ return [
         ],
 
         'single' => [
-            //'driver' => 'single',
-            'driver' => 'errorlog',
+            'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
